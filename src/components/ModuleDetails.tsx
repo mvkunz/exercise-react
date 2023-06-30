c86e77a2f41ad082f3e2a3a6e74a0de40ed1f3e0
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-06-30 11:00:49 -0300

    made the ModuleDetais component

---
+++ src/components/ModuleDetails.tsx
@@ -0,0 +1,13 @@
+function ModuleDetails() {
+  return (
+    <>
+    <li>Fundamentos</li>
+    <li>Front-end</li>
+    <li>Back-end</li>
+    <li>Ciência da Computação</li>
+    <li>Softskills</li>
+    </>
+  )
+}
+
+export default ModuleDetails;
