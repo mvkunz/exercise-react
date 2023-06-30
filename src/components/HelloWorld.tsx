70bfeb372025739038c3727d95266ab299589ba0
Maria Vitória Kunz <mvkunz.dev@gmail.com>
2023-06-30 11:06:53 -0300

    function calculateAge

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,11 +1,16 @@
 import Title from './Title';
 import ModuleDetails from './ModuleDetails';
+import { age } from '../utils/age';
+
 
 function HelloWorld() {
+  const idade = age('17/02/1997');
+
   return (
     <>
       <Title />
       <ModuleDetails />
+      <p>Minha idade atual é: {idade}</p>
     </>
   )
 }
---
+++ src/utils/age.ts
@@ -0,0 +1,12 @@
+export function age(birthday: string): number {
+  const [day, month, year] = birthday.split('/');
+  const birthDate = new Date(+year, +month - 1, +day);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age -= 1;
+  }
+  return age;
+}
+// O Birthday está como string pq passaremos a data como uma string, mas o retorno será em number.
